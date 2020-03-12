3e5ee6b290283871f4c8ac8e07fb78a2139ab27c
Laurentiu Amagdei <lau32on@gmail.com>
2020-03-12 18:54:25 +0100

    Rename starwars-store to coffee-store

---
+++ apps/coffee-store/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'apps-coffee-store',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/apps/coffee-store'
+};
--- apps/starwars-store/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'apps-starwars-store',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/apps/starwars-store'
-};
